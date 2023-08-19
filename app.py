4c8ac696c37e37cbcc1f8090c75c1b342a2f2cc4
Matthew <matthewyanglele@gmail.com>
2023-08-19 18:58:06 -0400

    added preview

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
+
+import os
+
 from whisper_functions import WhisperFunctions
 from split_audio import AudioSplitter
 from custom_timer import Timer
@@ -28,18 +31,19 @@ def result():
     full_transcription = session['transcription']
     wavFile = session['wavFile']
     split_audio_from_transcription(wavFile, full_transcription)
-    return f'elapsed time: {timer.stop()} secs' + load_results()
 
-def load_results():
-    transcription = session['transcription']
-    
-    # segment_folder = 
+    segment_folder_name = 'segments'
+    static_segment_folder_name = os.path.join("static", segment_folder_name)
+    wavFiles = [url_for('static', filename=segment_folder_name + "/" + f) for f in os.listdir(static_segment_folder_name) if os.path.isfile(os.path.join(static_segment_folder_name, f))]
+    transcriptTexts = [segment['text'] for segment in full_transcription['segments']]
+
+    return render_template('results.html', elapsed_time=timer.stop(), wavFiles_transcriptTexts=zip(wavFiles, transcriptTexts))
 
 def split_audio_from_transcription(filename, transcription):
     audio_splitter = AudioSplitter(filename)
     for index, segment in enumerate(transcription['segments']):
         segment_segment = audio_splitter.segment_audio(segment["start"], segment["end"])
-        audio_splitter.export_audio(segment_segment, f'segments/chunk{index}.wav')
+        audio_splitter.export_audio(segment_segment, f'static/segments/chunk{index}.wav')
 
 
 if __name__ == '__main__':
